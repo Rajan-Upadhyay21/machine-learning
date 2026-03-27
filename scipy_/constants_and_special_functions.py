aa67db9f5a6b00aa87edcba3d13ad5314ab443f6
Rajan Upadhyay <rajanupadhyay2106@gmail.com>
2026-03-27 18:55:36 -0500

    Create constants_and_special_functions.py

---
+++ scipy_/constants_and_special_functions.py
@@ -0,0 +1,18 @@
+# ---------------------------------------------------------
+# Program: SciPy Constants and Special Functions
+# Description:
+# This program demonstrates some basic SciPy constants
+# and special mathematical functions.
+# ---------------------------------------------------------
+
+from scipy import constants, special
+
+# Display mathematical and physical constants
+print("Value of pi:", constants.pi)
+print("Speed of light:", constants.c)
+print("Gravitational constant:", constants.G)
+
+# Special mathematical functions
+print("\nFactorial of 5:", special.factorial(5))
+print("Square root of 16:", special.sqrt(16))
+print("Exponential of 2:", special.exp10(2))

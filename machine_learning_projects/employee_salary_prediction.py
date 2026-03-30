7213d8861d0081a7dcb18ae09912bba3a2100409
Rajan Upadhyay <rajanupadhyay2106@gmail.com>
2026-03-30 16:24:29 -0500

    Create employee_salary_prediction.py

---
+++ machine_learning_projects/employee_salary_prediction.py
@@ -0,0 +1,32 @@
+from sklearn.datasets import make_regression
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+
+X, y = make_regression(
+    n_samples=400,
+    n_features=4,
+    noise=12,
+    random_state=42
+)
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+print("First 10 Actual Salary Values:")
+print(y_test[:10])
+
+print("\nFirst 10 Predicted Salary Values:")
+print(y_pred[:10])
+
+print("\nMean Squared Error:")
+print(mean_squared_error(y_test, y_pred))
+
+print("\nR2 Score:")
+print(r2_score(y_test, y_pred))

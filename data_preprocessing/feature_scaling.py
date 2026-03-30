aec1af3aa2dc210bc1c6100486d32215c31831e7
Rajan Upadhyay <rajanupadhyay2106@gmail.com>
2026-03-30 16:28:15 -0500

    Create feature_scaling.py

---
+++ data_preprocessing/feature_scaling.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+
+data = {
+    "Age": [20, 25, 30, 35, 40],
+    "Salary": [30000, 50000, 70000, 90000, 110000]
+}
+
+df = pd.DataFrame(data)
+
+print("Original Dataset:")
+print(df)
+
+standard_scaler = StandardScaler()
+standard_scaled = standard_scaler.fit_transform(df)
+
+standard_scaled_df = pd.DataFrame(standard_scaled, columns=df.columns)
+print("\nStandard Scaled Data:")
+print(standard_scaled_df)
+
+minmax_scaler = MinMaxScaler()
+minmax_scaled = minmax_scaler.fit_transform(df)
+
+minmax_scaled_df = pd.DataFrame(minmax_scaled, columns=df.columns)
+print("\nMin-Max Scaled Data:")
+print(minmax_scaled_df)

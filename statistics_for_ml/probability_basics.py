8de1aeed8d490f468de764018fd6196b6b59662e
Rajan Upadhyay <rajanupadhyay2106@gmail.com>
2026-04-06 14:28:51 -0500

    Create probability_basics.py

---
+++ statistics_for_ml/probability_basics.py
@@ -0,0 +1,8 @@
+sample_space = [1, 2, 3, 4, 5, 6]
+event_even = [2, 4, 6]
+
+probability_even = len(event_even) / len(sample_space)
+
+print("Sample Space:", sample_space)
+print("Event (Even Numbers):", event_even)
+print("Probability of Even Number:", probability_even)
